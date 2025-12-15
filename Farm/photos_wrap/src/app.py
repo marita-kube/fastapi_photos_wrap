c009f8a99aa5e52aceb42e683872c942e5afc8af
marita-kube <mwangimartha808@gmail.com>
2025-12-15 23:02:23 +0300

    define db schemas and app upload functionality

---
+++ Farm/photos_wrap/src/app.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
+from src.schemas import PostCreate, PostResponse
+from src.db import Post, create_database_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy import select
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+	await create_database_tables()
+	yield
+
+"""Automatically run lifespan function to create the database models"""
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_file(
+		file: UploadFile = File(...),
+		caption: str = Form(""),
+		session: AsyncSession = Depends(get_async_session)
+):
+
+	post = Post(
+		caption=caption,
+		url="dummy url",
+		file_type="photo",
+		file_name= "dummy name"
+
+	)
+	session.add(post)
+	await session.commit()
+	await session.refresh(post)
+	return post
+
+@app.get("/feed")
+async def get_feed(
+		session: AsyncSession = Depends(get_async_session)
+):
+	result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+	posts = [row[0] for row in result.all()]
+
+	posts_data = []
+
+	for post in posts:
+		posts_data.append(
+			{
+			"id": str(post.id),
+			"caption": post.caption,
+			"url": post.url,
+			"file_type": post.file_type,
+			"file_name": post.file_name,
+			"created_at": post.created_at.isoformat()
+		}
+	)
+
+	return {"posts": posts_data}
